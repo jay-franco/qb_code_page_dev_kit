3bb13410531fb8ae98225a04c00ec7e956750c7b
SimonH <simonh@jfranco.com>
2025-08-20 12:22:39 -0400

    refactor conf and add multi env support

--- pageupdate.py
+++ pageupdate.py
@@ -1,45 +1,58 @@
 import requests,sys, os
-import pageupdate_conf as authentication
+import argparse
+import json
 import xml.etree.ElementTree as etree
 
-qbUserToken = authentication.USER_TOKEN
-qbRealmHost = authentication.QB_REALM_HOST
-qbBaseURL = authentication.QB_BASE_URL
-qbApplicationDBID = authentication.QB_APPLICATION_DBID
+parser = argparse.ArgumentParser(description='Update or create QuickBase code pages')
+parser.add_argument('--filename', help='File to upload')
+parser.add_argument('--pageid', type=int, help='Page ID for updating existing page')
+parser.add_argument('--env', help='Environment to use (overrides current_environment in config)')
 
-pyFileId = None
-pyFileName = None
-# Count the number of passed in arguments
-argument_count = len(sys.argv) - 1
+args = parser.parse_args()
 
-if argument_count == 0:
-    most_recent_file = max((file for file in os.listdir('.') if file.endswith(('.html','.js'))), key=os.path.getmtime)
-    page = authentication.pages.get(most_recent_file)
-    pyFileName = most_recent_file
-    pyFileId = page
-elif argument_count == 2:
-    pyFileName = sys.argv[1]
-    pyFileId = sys.argv[2]
+# Load configuration from JSON
+with open('pageupdate_config.json', 'r', encoding='utf-8') as config_file:
+    config = json.load(config_file)
+    
+# Use specified environment or fall back to current_environment
+current_env = args.env if args.env else config["current_environment"]
 
-if pyFileId == None or pyFileName == None:
-    print("Invalid number of arguments")
+if current_env not in config["environments"]:
+    print(f"Error: Environment '{current_env}' not found in config")
+    print(f"Available environments: {', '.join(config['environments'].keys())}")
+    sys.exit()
+
+env_config = config["environments"][current_env]
+
+qbUserToken = env_config["USER_TOKEN"]
+qbRealm = env_config["QB_REALM"] 
+qbRealmHost = f"{qbRealm}.quickbase.com"
+qbBaseURL = f"https://{qbRealm}.quickbase.com"
+qbApplicationDBID = env_config["QB_APPLICATION_DBID"]
+pages_config = env_config["pages"]
+
+pyFileName = args.filename
+pyFileId = args.pageid
+isNewPage = False
+
+if not pyFileName:
+    pyFileName = max((file for file in os.listdir('.') if file.endswith(('.html','.js'))), key=os.path.getmtime)
+
+if not pyFileId:
+    pyFileId = pages_config.get(pyFileName)
+    if not pyFileId:
+        isNewPage = True
+
+if not pyFileName:
+    print("No file specified and no suitable files found")
     sys.exit()
 
 
 class DatabaseClient:
-    def __init__(self):
-        self.qb_dbid = getattr(self, 'qb_dbid', None) 
-        self.field_values = {}
-        self.apptoken = qbUserToken
-        self.realmhost = qbRealmHost
-        self.base_url = qbBaseURL
-        self.application_dbid = qbApplicationDBID
-        self.session = requests.Session()
-
     def add_replace_db_pages(self):
         
         # Request URL Handler
-        url = self.base_url + '/db/' + qbApplicationDBID
+        url = qbBaseURL + '/db/' + qbApplicationDBID
 
         # Generate Headers For Request
         headers = {
@@ -52,12 +65,20 @@ class DatabaseClient:
         with open(file_path, 'r', encoding='utf-8') as file:
             contents = file.read()
         
-        # Create A Request Dictionary
-        request_dict = {
-            'pageid': pyFileId,
-            'usertoken': qbUserToken,
-            'pagebody': contents,
-        }
+        # Create A Request Dictionary - different params for new vs existing pages
+        if isNewPage:
+            request_dict = {
+                'pagename': pyFileName,
+                'pagetype': '1',
+                'usertoken': qbUserToken,
+                'pagebody': contents,
+            }
+        else:
+            request_dict = {
+                'pageid': pyFileId,
+                'usertoken': qbUserToken,
+                'pagebody': contents,
+            }
 
         # Convert to XML 
         xml_data = self._build_request(**request_dict)
@@ -70,9 +91,17 @@ class DatabaseClient:
         parsed = etree.fromstring(response)
         error_code = int(parsed.findtext('errcode'))
         if error_code == 0:
-            print(f"Success! {pyFileName} Has Been Updated And Completed")
+            if isNewPage:
+                new_page_id = int(parsed.findtext('pageID'))
+                print(f"Success! New page '{pyFileName}' created with ID: {new_page_id}")
+                print(f"View page: https://{qbRealm}.quickbase.com/db/{qbApplicationDBID}?pageID={new_page_id}&a=dbpage")
+                self._add_page_to_config(pyFileName, new_page_id)
+            else:
+                print(f"Success! {pyFileName} Has Been Updated And Completed")
+                print(f"View page: https://{qbRealm}.quickbase.com/db/{qbApplicationDBID}?pageID={pyFileId}&a=dbpage")
         else:
-            print("Error Code", error_code)
+            error_text = parsed.findtext('errtext', 'Unknown error')
+            print(f"Error Code {error_code}: {error_text}")
 
     def _build_request(self, **kwargs):
         # Convert the dictionary to an XML string with CDATA around pagebody
@@ -84,6 +113,23 @@ class DatabaseClient:
                 request_xml += f"<{key}>{value}</{key}>"
         request_xml += "</qdbapi>"
         return request_xml
+
+    def _add_page_to_config(self, filename, page_id):
+        config_file = 'pageupdate_config.json'
+        
+        with open(config_file, 'r+', encoding='utf-8') as file:
+            config = json.load(file)
+            
+            # Add the new page to current environment
+            current_env = config["current_environment"]
+            config["environments"][current_env]["pages"][filename] = page_id
+            
+            # Write updated config back
+            file.seek(0)
+            json.dump(config, file, indent=2)
+            file.truncate()
+        
+        print(f"Added '{filename}' : {page_id} to {config_file}")
 
 # Example usage
 client = DatabaseClient()
